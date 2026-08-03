eb7655aef1a53857d59834e9200d165e0c9221dc
Abhishek Lad <abhisheklad963@gmail.com>
2026-08-04 00:14:10 +0530

    Time: 1 ms (99.88%), Space: 47.5 MB (11.05%) - LeetHub

--- 3033-modify-the-matrix/3033-modify-the-matrix.java
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -1,21 +1,32 @@
 class Solution {
     public int[][] modifiedMatrix(int[][] matrix) {
-        
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        for ( int j = 0 ; j < n ; j++){
-            int mx = -1 ;
-            for ( int i = 0 ; i < m ; i++){
-                mx = Math.max(mx , matrix[i][j]);
+        int[] colMax = new int[cols];
+
+        // Step 1: Find maximum value of each column
+        for(int j = 0; j < cols; j++) {
+            int max = Integer.MIN_VALUE;
+
+            for(int i = 0; i < rows; i++) {
+                max = Math.max(max, matrix[i][j]);
             }
 
-            for ( int i = 0 ; i < m ; i++){
-                if (matrix[i][j] == -1 ){
-                    matrix[i][j] = mx ;
+            colMax[j] = max;
+        }
+
+        // Step 2: Replace -1 with column maximum
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+
+                if(matrix[i][j] == -1) {
+                    matrix[i][j] = colMax[j];
                 }
             }
         }
-        return matrix ;
+
+        return matrix;
     }
 }
+    
